392479ace4e5846a283ca11bd069badca8fce743
m3k0813 <m3k0813@gmail.com>
2021-07-14 21:43:58 +0900

    Daum mail title

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,17 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Main {
 
@@ -11,16 +22,27 @@ public class Main {
         DaumLogin();
     }
 
-    static public void init(){
+    static public void init() {
         System.setProperty("webdriver.chrome.driver", "../selenium-java-3.141.59/chromedriver.exe");
         driver = new ChromeDriver();
     }
 
-    static public void DaumLogin(){
+    static public void DaumLogin() throws InterruptedException {
         driver.get("https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fmail.daum.net%2F");
 
         driver.findElement(By.id("id")).sendKeys("");
         driver.findElement(By.id("inputPwd")).sendKeys("");
         driver.findElement(By.className("btn_comm")).click();
+        Thread.sleep(3000);
+
+        List<WebElement> titles;
+        titles = driver.findElements(By.cssSelector("#mailList a.link_subject"));
+//        titles = driver.findElementsByCssSelector("div#mailList a.link_subject");
+        System.out.println(titles.size());
+        if (!titles.isEmpty()) {
+            for (WebElement title : titles) {
+                System.out.println(title.getText());
+            }
+        }
     }
 }
